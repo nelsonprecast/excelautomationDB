4c9d99135ee50a742c873e7c64c482ed84fa0d4d
Majid Ali <majid.ali@codehubsys.com>
2023-09-25 20:19:58 +0500

    Added New Columns

--- ExcelAutomation/ExcelAutomation/dbo/Tables/PlanElevationText.sql
+++ ExcelAutomation/ExcelAutomation/dbo/Tables/PlanElevationText.sql
@@ -3,6 +3,8 @@
     [Text]        NVARCHAR (1000) NOT NULL,
     [ProjectId]   INT             NOT NULL,
     [CreatedDate] DATETIME        NOT NULL,
+    [ImageSnipPath] VARCHAR(500) NULL, 
+    [PageRefImagePath] VARCHAR(500) NULL, 
     CONSTRAINT [PK_PlanElevationText] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
